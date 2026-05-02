a807e4a17e82f2332ade1987e97b38605ab00bd3
Prashanthcoder <tp120113@gmail.com>
2026-05-02 22:50:41 +0530

    Configuration of the container by spring mvc

--- src/main/java/com/configuration/Myconfig.java
+++ src/main/java/com/configuration/Myconfig.java
@@ -1,10 +1,29 @@
 package com.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan(basePackages =  "com.controller,com.model")
+@ComponentScan(basePackages = "com.controller")
 public class Myconfig {
 
+    // Tells Spring MVC where JSP files live
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix("");
+        return resolver;
+    }
+
+    // REQUIRED for resume file upload to work
+    // StandardServletMultipartResolver uses the built-in Servlet 3.0 multipart support
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 }
